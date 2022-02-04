11499c45b24be628f884d51b4c6bd429dd0723f1
trondi <trond746@gmail.com>
2022-02-05 01:15:16 +0900

    json

--- notes/json.js
+++ notes/json.js
@@ -28,7 +28,7 @@ json = JSON.stringify(rabbit, ['name', 'color', 'size']);
 // 해당하는 property만 출력 가능
 console.log(json);
 
-json = JSON.stringify(rabbit, (key, value) => {
+json = JSON.stringify(rabbit, (key, value) => {// callback 함수를 통해서 전달받을 수 있음
   console.log(`key: ${key}, value: ${value}`);
   return key === 'name' ? 'ellie' : value;
 });
@@ -37,15 +37,21 @@ console.log(json);
 // 2. JSON to Object
 // parse(from json)
 console.clear();
+
 json = JSON.stringify(rabbit);
-console.log(json);
+console.log(json);//birthdate -> string
+
 const obj = JSON.parse(json, (key, value) => {
   console.log(`key: ${key}, value: ${value}`);
   return key === 'birthDate' ? new Date(value) : value;
+  //키가 만약 birthdate면 새로운 오브젝트를 만들거나 아니면 그냥 원래 값을 쓸것
 });
 console.log(obj);
-rabbit.jump();
-// obj.jump();
+rabbit.jump(); //rabbit의 오브젝트는 바로 출력 : can jump!
+// obj.jump(); 
+//변환한 오브젝트에서 함수는 직렬화 할때 포함되지 않으므로 쓸 수 없음
 
-console.log(rabbit.birthDate.getDate());
-console.log(obj.birthDate.getDate());
+//API 사용
+console.log(rabbit.birthDate.getDate());//29
+console.log(obj.birthDate.getDate());//error
+//obj.birthDate 이 string으로 오브젝트에 할당
